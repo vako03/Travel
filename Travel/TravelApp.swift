9fd793136e14ccc0c73fd8219a47b5312b7c8335
Vako <valeri.mekhashishvili97@gmail.com>
2024-05-30 00:52:03 +0400

    Initial Commit

---
+++ Travel/TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelApp.swift
+//  Travel
+//
+//  Created by valeri mekhashishvili on 30.05.24.
+//
+
+import SwiftUI
+
+@main
+struct TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
